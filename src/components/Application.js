06246dc67ec8f7b867ffd7706048a3f09bbf032f
NotJenniferConnelly <antoineincognito@hotmail.com>
2021-03-26 19:24:40 -0400

    implemented rotating cube

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,36 @@
-import React from "react"
+import React, { useRef } from "react"
+
+import styled from "styled-components"
+import { Canvas, useFrame } from "react-three-fiber"
+import { Box } from "@react-three/drei"
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: #333;
+`
+
+const Boxed = () => {
+  const mesh = useRef(null)
+  useFrame(() => { mesh.current.rotation.x = mesh.current.rotation.y += 0.01 })
+
+  return (
+    <mesh ref={mesh}>
+      <Box>
+        <meshStandardMaterial attach="material"/>
+      </Box>
+    </mesh>
+  )
+}
 
 const Application = () => {
   return (
-    <div>Hello World</div>
+    <Container>
+      <Canvas>
+        <ambientLight intensity={0.2} />
+        <Boxed />
+      </Canvas>
+    </Container>
   )
 }
 
