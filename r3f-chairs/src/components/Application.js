c613e466cdcd083006cc199510f31c5e0387148a
NotJenniferConnelly <antoineincognito@hotmail.com>
2021-03-28 15:51:21 -0400

    rendered a chair

--- r3f-chairs/src/components/Application.js
+++ r3f-chairs/src/components/Application.js
@@ -1,15 +1,39 @@
-import React from "react"
+import React, { Suspense } from "react"
 
 import styled from "styled-components"
 
 import Header from "./Header"
 import { Canvas } from "react-three-fiber"
+import { Section } from "./Section"
+import { Html, useGLTF } from "@react-three/drei"
 
 const Container = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 100vw;
   height: 100vh;
+  max-height: 100vh;
   background: #444;
 `
+
+const Model = () => {
+  const gltf = useGLTF('/armchairYellow.gltf', true)
+  return (
+    <primitive object={gltf.scene} dispose={null} />
+  )
+}
+
+const HTMLContent = () => {
+  return (
+    <Section factor={1.5} offset={1}>
+      <group position={[-5, 250, 0]}>
+        <mesh>
+          <Model />
+        </mesh>
+      </group>
+    </Section>
+  )
+}
 
 const Application = () => {
   return (
@@ -19,6 +43,9 @@ const Application = () => {
         colorManagement
         camera={{ position: [0, 0, 120], fov: 70 }}
       >
+        <Suspense fallback={null}>
+          <HTMLContent />
+        </Suspense>
       </Canvas>
     </Container>
   )
